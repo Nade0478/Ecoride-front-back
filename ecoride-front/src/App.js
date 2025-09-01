d6c8c1243dc68113f0501b709408c43c60d61821
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-09-01 04:40:54 +0200

    commit

--- ecoride-front/src/App.js
+++ ecoride-front/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Blog from "./pages/Blog";
-import Home from "./pages/home";
-
+import Home from "./pages/Home";
 
 const App = () => {
   return (
